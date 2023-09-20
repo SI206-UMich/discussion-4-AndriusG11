f3199e2a4ecedc529f9c1ff1dc74520e91fd181c
Andrius <andriusg@umich.edu>
2023-09-20 14:06:09 -0400

    Commit 2

--- discussion4_starter.py
+++ discussion4_starter.py
@@ -1,48 +1,27 @@
 class Rectangle():
-    # Create the constructor "__init__" method
-    # Arguments: width (an ingeter), height (an integer)
-    # 
-    # It sets an instance variable, "width" to the passed argument, width
-    # It sets an instance variable, "height" to the passed argument, height
-
-    # YOUR CODE HERE
+    
     def __init__(self, width, height):
         self.width = width
         self.height = height
 
 
 
-    # Create the "__str__" method
-    #
-    # It returns a string, 
-    #       "A rectangle with width ____ and height ____"
-
-    # YOUR CODE HERE
+    
     def __str__(self):
         return f"A rectangle with width {self.width} and height {self.height}"
 
 
 
-    # Create the "verify_input" method
-    #
-    # It returns a boolean
-    #       True if the width and height are positive numbers
-    #       False otherwise
-
-    # YOUR CODE HERE
+    
     def verify_input(self):
         if self.width > 0 and self.height > 0:
             return True
         return False
 
+    
+    
 
-
-    # Create the "area" method
-    #
-    # It first verifies inputs and return "Invalid input" if they are invalid.
-    # Otherwise, it returns the area of the rectangle.
-
-    # YOUR CODE HERE
+    
     def area(self):
         input = self.verify_input()
         if input == False:
@@ -51,12 +30,7 @@ class Rectangle():
 
 
 
-    # Create the "perimeter" method
-    #
-    # It first verifies inputs and return "Invalid input" if they are invalid.
-    # Otherwise, it returns the perimeter of the rectangle.
-
-    # YOUR CODE HERE
+    
     def perimeter(self):
         input = self.verify_input()
         if input == False:
